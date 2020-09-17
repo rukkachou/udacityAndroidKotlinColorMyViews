2ded0f64fe033fd3773974df714d82b3a4c482e8
rukkachou <rukka.chou@gmail.com>
2020-09-18 02:56:44 +0800

    after ch29

--- app/src/main/java/com/example/udacity/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/udacity/colormyviews/MainActivity.kt
@@ -16,7 +16,8 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         val viewList = listOf(box_one_text, box_two_text,
             box_three_text, box_four_text, box_five_text,
-            constraint_layout)
+            constraint_layout,
+            red_button, yellow_button, green_button)
         for (view in viewList) {
             view.setOnClickListener { makeColored(it) }
         }
@@ -24,14 +25,18 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeColored(view: View) {
         when (view.id) {
-            R.id.box_one_text -> view.setBackgroundColor(Color.LTGRAY)
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
 
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_red_dark)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_blue_dark)
 
-            else -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.myRed)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.myYellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.myGreen)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
 }
