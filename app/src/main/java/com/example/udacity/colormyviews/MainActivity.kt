f70389579e227cd9588b6ce7d43040afedccfb8e
rukkachou <rukka.chou@gmail.com>
2020-09-18 02:06:45 +0800

    after ch27

--- app/src/main/java/com/example/udacity/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/udacity/colormyviews/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.udacity.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val viewList = listOf(box_one_text, box_two_text,
+            box_three_text, box_four_text, box_five_text,
+            constraint_layout)
+        for (view in viewList) {
+            view.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.LTGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_red_dark)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_blue_dark)
+
+            else -> view.setBackgroundColor(Color.DKGRAY)
+        }
     }
 }
